32891eb04089fd4bb45889d406ece7c3ac0aff91
Philip Kreismayr <philip.kreismayr@lemon42.com>
2018-11-27 23:00:42 +0100

    add sounds

---
+++ sound/control.py
@@ -0,0 +1,44 @@
+from subprocess import call
+
+def text1():
+  call(['omxplayer', '-o','local','assets/text1.wav'])  
+
+def text2():
+  call(['omxplayer', '-o','local','assets/text2.wav'])  
+
+def text3():
+  call(['omxplayer', '-o','local','assets/text3.wav']) 
+
+def text4():
+  call(['omxplayer', '-o','local','assets/text4.wav']) 
+
+def text5():
+  call(['omxplayer', '-o','local','assets/text5.wav']) 
+
+def text6():
+  call(['omxplayer', '-o','local','assets/text6.wav']) 
+
+def text7():
+  call(['omxplayer', '-o','local','assets/text7.wav']) 
+
+def lied1():
+  call(['omxplayer', '-o','local','assets/lied1.wav']) 
+
+def lied2():
+  call(['omxplayer', '-o','local','assets/lied2.m4a']) 
+
+def lied3_1():
+  call(['omxplayer', '-o','local','assets/lied3_1.wav']) 
+
+def lied3_2():
+  call(['omxplayer', '-o','local','assets/lied3_2.wav']) 
+
+def lied3_3():
+  call(['omxplayer', '-o','local','assets/lied3_3.wav']) 
+
+def lied4():
+  call(['omxplayer', '-o','local','assets/lied4.wav']) 
+
+def lied5():
+  call(['omxplayer', '-o','local','assets/lied5.m4a']) 
+
